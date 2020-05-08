149a922100d22aeeefe0a732a2a6f66ae8914a4f
Mohanraj kanthasamy <mohanraj.kandhasamy@crystaldelta.com>
2020-05-08 18:19:10 +0530

    implemented bootstarp and navbar section

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodoList from "./components/todo-list.component";
+
+import logo from "./logo.svg";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+            <a className="navbar-brand" href="www.google.com" target="_blank">
+              <img src={logo} width="30" height="30" alt="react-logo" />
+              <Link to="/" className="navbar-brand">BILLING Systems</Link>
+              <div className="collpase nav-collapse">
+                <ul className="navbar-nav mr-auto">
+                  <li className="navbar-item">
+                    <Link to="/" className="nav-link">Products</Link>
+                  </li>
+                  <li className="navbar-item">
+                    <Link to="/create" className="nav-link">Create product</Link>
+                  </li>
+                </ul>
+              </div>
+            </a>
+          </nav>
+          <Route path="/" exact component={TodoList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
